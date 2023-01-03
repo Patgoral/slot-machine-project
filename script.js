78428a503d1dc9d7facffc7455f50e07b3a231a5
Patrick Goral <patgoral@gmail.com>
2023-01-03 17:18:34 -0500

    end of day work 1/3

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 const machineEl = document.getElementById('crocs-machine')
 const cashBtn = document.getElementById('cash-out')
 const playBtn = document.getElementById('play-game')
-let wagerInput = document.getElementById('wager-input')
-let playerBalance = document.getElementById('balance-box')
+let wagerInput = document.querySelector('input').value
+let playerBalance = document.getElementById('money').textContent
 
-function playGame(){
+
+
+
+//Creates Game
+playGame = () => {
 const images = [
-    '/img/crocscamo.png',
-    '/img/crocsorg.jpg',
-    '/img/crocsred.jpg'
+    'img/crocscamo.png',
+    'img/crocsorg.jpg',
+    'img/crocsred.jpg'
 ];
 
 const camo = images[0];
@@ -36,26 +40,44 @@ function randomize (arr)
     }
 
 let arr = [camo, red, orange, orange, camo, red, red, camo, orange];
-randomize (arr);
+randomize(arr);
 printArray(arr);
 let slotArray = (arr)
 
+//Pushes Random Img to slot
 let slotOne = slotArray[0]
 let slotTwo = slotArray[1]
 let slotThree = slotArray[2]
 document.querySelector('#slot-one').src = slotOne;
 document.querySelector('#slot-two').src = slotTwo;
 document.querySelector('#slot-three').src = slotThree;
+}
+ 
 
-console.log(slotArray)
-console.log(slotOne)
-console.log(slotTwo)
-console.log(slotThree)
 
+
+
+//Scoring Logic
+winEvent = () => {
+    if (playerBalance > 0) {
+        playerBalance = 500;
+    if (slotOne == camo && slotTwo == camo && slotTwo == camo) {
+        playerBalance += wagerInput * 5;
+    } else if (slotArray[0] == orange && slotArray[1] == orange && slotArray[2] == orange) {
+        playerBalance += wagerInput * 3;
+    } else if (slotArray[0] == red && slotArray[1] == red && slotArray[2] == red) {
+        playerBalance += wagerInput * 2;
+    } else {
+         playerBalance * 5;
+    }
+    document.getElementById("money").innerHTML = playerBalance.toFixed(2);
+    }
+    else 
+        alert ('You Are Out Of Cash!')
+        
 }
 
 
-    
 
-
-playBtn.addEventListener('click', playGame)
+playBtn.addEventListener('click', playGame, )
+console.log(playerBalance)
